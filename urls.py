9b26d6aa40b16f496ef29c2c99fe8621c3896902
lenovo <arunghosh@gmail.com>
2015-03-06 12:11:54 +0530

    test commit

--- urls.py
+++ urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.views.generic.base import TemplateView
 from django.contrib import admin
 
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
