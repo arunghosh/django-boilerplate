abda3cb621f2e59a8877281ab7f74779a4c28f87
lenovo <arunghosh@gmail.com>
2015-04-04 10:48:06 +0530

    added email unique check & style change

--- apps/account/managers.py
+++ apps/account/managers.py
@@ -7,6 +7,8 @@ class UserManager(BaseUserManager):
         if not email:
             raise ValueError('Users must have an email address')
 
+        self._check_unique_email(email)
+
         user = self.model(
             email=self.normalize_email(email),
             first_name=first_name,
@@ -29,6 +31,13 @@ class UserManager(BaseUserManager):
 
     def get_by_email(self, email):
         return self.get_queryset().get(email=email)
+
+    def _check_unique_email(self, email):
+        try:
+          self.get_queryset().get(email=email)
+          raise ValueError("This email is already registered")
+        except self.model.DoesNotExist:
+          return True
 
 
 class EmailAddressManager(models.Manager):
