284ea344b26ea34663e1691d1f6f5bc714bc5d31
Abdul Shukoor <abdulshukoorpk@gmail.com>
2015-04-28 15:44:27 +0530

    modified utils.tests

--- apps/utils/tests.py
+++ apps/utils/tests.py
@@ -8,8 +8,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-# Create your tests here.
-
 
 class SeleniumTestCase(TestCase):
 
