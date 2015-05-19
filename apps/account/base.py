258d10f19fa5f76b3815e7cee6d761a58550e534
lenovo <arunghosh@gmail.com>
2015-05-19 18:49:17 +0530

    refactoring of Base UserProfile

--- apps/account/base.py
+++ apps/account/base.py
@@ -15,11 +15,25 @@ GENDER_OPTS = (
   (0, "--"),
   (1, "Male"),
   (2, "Female"),
-  (3, "Other"),
 )
 
 
-class AbstractUserProfile(AbstractTimestampModel):
+class UserProfileManager(models.Manager):
+
+    def register(self, email, password, first_name, last_name, mobile):
+        user = User.objects.create_user(
+            email= email,
+            password= password,)
+        profile = self.create(
+            first_name=first_name,
+            last_name=last_name,
+            mobile=mobile,
+            email=email,
+            user=user,)
+        return profile
+
+
+class AbstractUserProfileModel(AbstractTimestampModel):
     user = models.OneToOneField(User, related_name="%(class)s_profile", null=True, blank=True)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
@@ -30,34 +44,45 @@ class AbstractUserProfile(AbstractTimestampModel):
     country = models.CharField(max_length=64, null=True, blank=True)
     gender = models.PositiveSmallIntegerField(choices=GENDER_OPTS, default=0, verbose_name="Gender")
     dob = models.DateField(null=True, blank=True)
+    password = 'abcd1234'
+
+    objects = UserProfileManager()
 
     def add_log(self):
         return self.user.add_log()
 
     @property
-    def name(self):
+    def full_name(self):
         return self.first_name + " " + self.last_name
 
     @transaction.atomic
     def save(self, *args, **kwargs):
-        super(AbstractUserProfile, self).save(*args, **kwargs)
-        self._upsert_user(*args, **kwargs)
+        super(AbstractUserProfileModel, self).save(*args, **kwargs)
+        self._create_user()
+        self._update_user()
 
-    def _upsert_user(self, *args, **kwargs):
-        if not self.user:
-            self.user = User.objects.create_user(
-                email=self.email, 
-                first_name=self.first_name, 
-                last_name=self.last_name,
-                password='abcd1234')
-            super(AbstractUserProfile, self).save(*args, **kwargs)
+    def _update_user(self):
+        '''
+        Sync Data with user_profile and user
+        '''
         self.user.first_name = self.first_name
         self.user.last_name = self.last_name
         self.user.email = self.email
         self.user.save()
 
+    def _create_user(self, *args, **kwargs):
+        '''
+        When user is created via admin
+        '''
+        if not self.user:
+            self.user = User.objects.create_user(
+                email=self.email, 
+                password=self.password)
+            super(AbstractUserProfileModel, self).save(*args, **kwargs)
+
     class Meta:
         abstract = True
+
 
 
 class AbstractRegistrationForm(forms.Form):
@@ -76,9 +101,11 @@ class AbstractRegistrationForm(forms.Form):
 
     @abstractmethod
     def create_object(self, **kwargs):
-        ''' You can overrride object creation in the firn save method
+        ''' 
+        You can overrride object creation in the firn save method
         ex: CustomUser.objects.register(**kwargs)
         '''
+        # profile.register()
         pass
 
     def clean_email_confirm(self):
