45d9e844bd163dd97b99372822996fae94d3a136
lenovo <arunghosh@gmail.com>
2015-05-19 15:51:30 +0530

    updated account views import

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls import patterns, url
-from .views import LoginView, logout_view, email_confirm_view,  SendConfirmEmailView, SendPasswordResetMailView, PasswordResetView, RegisterView
+from . import views
 
 urlpatterns = patterns(
     '',
-    url(r'^login/$', LoginView.as_view(), name='login'),
-    url(r'^logout/$', logout_view, name='logout'),
-    url(r'^register/$', RegisterView.as_view(), name='register'),
-    # url(r'^register/$', UserProfileView.as_view(), name='register'),
-    url(r'^password/reset/(?P<id>[0-9]*)/(?P<key>[-\w\d]+)$', PasswordResetView.as_view(), name='password_reset'),
-    url(r'^password/send-reset/$', SendPasswordResetMailView.as_view(), name='send_password_reset'),
-    url(r'^email/send-verify/$', SendConfirmEmailView.as_view(), name='resend_email_confirm'),
-    url(r'^email/confirm/(?P<id>[0-9]*)/(?P<key>[-\w\d]+)$', email_confirm_view, name='email_confirm'),
+    url(r'^login/$', views.LoginView.as_view(), name='login'),
+    url(r'^logout/$', views.ogout_view, name='logout'),
+    url(r'^register/$', views.RegisterView.as_view(), name='register'),
+    url(r'^password/reset/(?P<id>[0-9]*)/(?P<key>[-\w\d]+)$', views.PasswordResetView.as_view(), name='password_reset'),
+    url(r'^password/send-reset/$', views.SendPasswordResetMailView.as_view(), name='send_password_reset'),
+    url(r'^email/send-verify/$', views.SendConfirmEmailView.as_view(), name='resend_email_confirm'),
+    url(r'^email/confirm/(?P<id>[0-9]*)/(?P<key>[-\w\d]+)$', views.email_confirm_view, name='email_confirm'),
 )
